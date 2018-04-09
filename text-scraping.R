b0ee55a2e4db566117359c363692582489dd5d0d
Sabreena <sabreenaabedin@d-172-25-79-25.dhcp.virginia.edu>
2018-04-09 13:57:43 -0400

    updated scraping'

--- text-scraping.R
+++ text-scraping.R
@@ -1,84 +1,97 @@
+################################
+### INSTALL/LIBRARY PACKAGES ###
+################################
 install.packages("rvest")
+install.packages("stringr")
 library(rvest)
+library(stringr)
+
+############################
+### FIND LINKS TO FOLLOW ###
+############################
+
+# DEFINE PAGE ROOT
+main.page <- read_html(x = "https://www.trendhunter.com/results?search=skin+care")
+
+# CREATING LIST OF LINKS
+urls <- main.page %>% 
+  html_nodes("a") %>% 
+  html_attr("href") # extract the URLs
+
+links <- main.page %>% 
+  html_nodes("a") %>% 
+  html_text() # extract the link text
+
+# Combine `links` and `urls` into a data.frame
+  #linkframe <- data.frame(links = links, urls = urls, stringsAsFactors = FALSE)
+linkframe <- data.frame(urls = urls, stringsAsFactors = FALSE)
+head(linkframe)
+linkframe <- linkframe[!is.na(linkframe)]
+
+
+pattern <- "/." # PATTERN INDICATES IT IS A LINK
+links_valid <- character(0) # create vector for valid links
+
+# how many are valid links?
+for(i in 1:length(linkframe)){
+  print(str_detect(linkframe[i], pattern))
+  if(str_detect(linkframe[i], pattern)){
+    links_valid <- c(linkframe[i])
+    print(paste0("Added link: ",linkframe[i])) #this part prints the links as needed
+  }
+}
+
+head(links_valid) #RETURNS A RANDOM LINK?????
+
+
+################################
+### SCRAPE AND WRITE TO FILE ###
+################################
 
 #write to file
 setwd("/Users/sabreenaabedin/Desktop/automation-tools/")
 sink("output.txt")
 
-#### MY ISSUE IS GETTING THIS ARTICLE TITLE TO GO THROUGH ALL THE LINKS AVAILABLE
-articleTitle
+### SCRAPE AND WRITE - THIS PART WORKS IF THE NAME IS CORRECT
 
-site <- html("https://www.trendhunter.com/results?search=skin+care")
-
-links <- site %>%
-  html_nodes("a") %>%
-  html_attr("href")
-
-df <-data.frame(link = character())
-df$link <- links
-
-for(i in 0:length(links)){
-  test = substring(links[i],0,1)
-  condition = compareEqual(test, "/")
-  if(condition[1] == TRUE){
-    ##df$link[i] <- links[i]
-    print(links[i])
-  } else {
-    
-  }
-}
-
-############ SCRAPE AND WRITE - THIS PART WORKS IF THERE'S A TITLE
-
-for(i in 15:length(links)){
-  name <- paste0("https://www.trendhunter.com", articleTitle)
-site <- read_html(name)
-
-#scrape html code
-subTitle <- site %>%
-  html_nodes(".subTitle > h2") %>%
-  html_text() 
-
-articleText <- site %>%
-  html_nodes(".articleText") %>%
-  html_text() 
-
-title <- site %>%
-  html_nodes(".titleSection > .cc") %>%
-  html_text() 
-
-author <- site %>%
-  html_nodes(".author__link") %>%
-  html_text() 
-
-date <- site %>%
-  html_nodes(".references > span") %>%
-  html_text() 
-
-# clean
-author <- gsub("\n\t","", author)
-author <- gsub("\t","", author)
-articleText <- gsub("\n","", articleText)
-articleText <- gsub("\t","", articleText)
-title <- gsub("\n","", title)
-title <- gsub("\t","", title)
-subTitle <- gsub("\n","", subTitle)
-subTitle <- gsub("\t","", subTitle)
-date <- gsub("\n","", date)
-date <- gsub("\t","", date)
-
-
-cat(title,";", subTitle,";", author,";", date,";",articleText, "\n")
+for(i in 0:length(links_final)){
+  name <- paste0("https://www.trendhunter.com", links_final[i]) #IF INVALID, WON'T WORK
+  site <- read_html(name)
+  
+  #scrape html code
+  subTitle <- site %>%
+    html_nodes(".subTitle > h2") %>%
+    html_text() 
+  
+  articleText <- site %>%
+    html_nodes(".articleText") %>%
+    html_text() 
+  
+  title <- site %>%
+    html_nodes(".titleSection > .cc") %>%
+    html_text() 
+  
+  author <- site %>%
+    html_nodes(".author__link") %>%
+    html_text() 
+  
+  date <- site %>%
+    html_nodes(".references > span") %>%
+    html_text() 
+  
+  # clean
+  author <- gsub("\n\t","", author)
+  author <- gsub("\t","", author)
+  articleText <- gsub("\n","", articleText)
+  articleText <- gsub("\t","", articleText)
+  title <- gsub("\n","", title)
+  title <- gsub("\t","", title)
+  subTitle <- gsub("\n","", subTitle)
+  subTitle <- gsub("\t","", subTitle)
+  date <- gsub("\n","", date)
+  date <- gsub("\t","", date)
+  
+  
+  cat(title,";", subTitle,";", author,";", date,";",articleText, "\n")
 }
 sink()
-
-
-
-
-
-#install.packages("compare")
-#library(compare)
-
-
-#condition.result()
-#condition[1]
